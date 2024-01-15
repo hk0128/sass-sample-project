d5832f9efa1276f7d26349f6912fd476dd545394
hiro <hironobu.kamata@adglobe.co.jp>
2024-01-15 12:35:59 +0900

    初回コミット

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <div id='speech-bubble-box'>
+        <div id='speech-bubble-a'>吹き出しA</div>
+        <div id='speech-bubble-b'>吹き出しB</div>
+        <div id='speech-bubble-c'>吹き出しC</div>
+      </div>
+
+      <div id='button-box'>
+        <button className='btn-a'>ボタン1</button>
+        <button className='btn-b'>ボタン2</button>
+        <button className='btn-c'>ボタン3</button>
+      </div>
+    </>
+  )
+}
+
+export default App
