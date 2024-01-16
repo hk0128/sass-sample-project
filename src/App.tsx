ca117ce9c871e01ba94818bb4489dbefbdbe34d3
hiro <hironobu.kamata@adglobe.co.jp>
2024-01-16 10:11:04 +0900

    ラジオボタン追加

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,15 @@ function App() {
         <button className='btn-b'>ボタン2</button>
         <button className='btn-c'>ボタン3</button>
       </div>
+
+      <div id='radio-box'>
+        <input type='radio' className='radio-style' name='sample' value='' />
+        <label>ラジオボタン1</label>
+        <input type='radio' className='radio-style' name='sample' value='' />
+        <label>ラジオボタン2</label>
+        <input type='radio' className='radio-style' name='sample' value='' />
+        <label>ラジオボタン3</label>
+      </div>
     </>
   )
 }
